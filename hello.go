b7c48bc036de4127e3370106388b99660559db73
Charita <charita@Charitas-MacBook-Air.local>
2019-07-22 18:34:54 -0400

    Data Types and Variables Examples

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world first Program\n")
+	fmt.Printf("hello, world first\n")
 }
---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int = 10
+	var s string = "Japan"
+	fmt.Println(i)
+	fmt.Println(s)
+}
